7c72f21da90d6f8539bbc02788be3a82d066ba0e
Forest Belton <fbelton@theore.tc>
2012-06-24 04:31:52 +0000

    Added some network code, and test code for the client

--- include/net.h
+++ include/net.h
@@ -1,5 +1,28 @@
 #ifndef COTITAN_NET_H_
 #define COTITAN_NET_H_
 
+#include <stdint.h>
+
+typedef struct {
+  uint16_t  type;
+  uint16_t  len;
+  uint8_t  *data;
+} cot_packet_t;
+
+typedef enum {
+  PKT_AUTH = 0x0000,
+  PKT_SAY  = 0x0001 
+} cot_pktid_t;
+
+/* Connects to the cotitan daemon on the given host:port. */
+int net_init(const char *host, const char *port);
+
+/* Authenticates as a certain user. */
+int net_auth(const char *user, const char *pass);
+
+/* Sends a chat message. */
+void net_say(char *msg);
+void net_sayf(const char *msg, ...);
+
 #endif
 
--- src/client.c
+++ src/client.c
@@ -1,4 +1,28 @@
-int main() {
+#include "net.h"
+#include "ui.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  char *user, *pass;
+  
+  ui_init();
+
+  /* Once ui_read_line works, we'll use this instead: */
+#if 0
+  user = ui_prompt(0, "Username: ");
+  pass = ui_prompt(1, "Password: ");
+#endif
+  
+  user = "apples";
+  pass = "test";
+  
+  net_init("localhost", "48581");
+  net_auth(user, pass);
+  net_say("This is a test");
+
   return 0;
 }
 
