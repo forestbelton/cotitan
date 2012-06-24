b72c16822bf86459b2446751bb847843f8c020a7
Forest Belton <fbelton@theore.tc>
2012-06-24 20:32:13 +0000

    Fixed DEBUG() on release builds and pulled it out into include/misc.h for general use

--- include/misc.h
+++ include/misc.h
@@ -18,51 +18,15 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 * THE SOFTWARE.
 */
-#include "misc.h"
-#include "net.h"
-#include "netc.h"
+#ifndef COTITAN_MISC_H_
+#define COTITAN_MISC_H_
 
-#include <stdarg.h>
-#include <string.h>
+#ifndef NDEBUG
+#  include <stdio.h>
+#  define DEBUG(msg) printf("%s\n", msg)
+#else
+#  define DEBUG(msg) do { } while(0)
+#endif
 
-sock_t clientfd;
-
-int netc_connect(const char *hostname, const char *port) {
-  struct addrinfo hints, *res;
-  
-  memset(&hints, 0, sizeof hints);
-  hints.ai_family   = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  
-  DEBUG("calling getaddrinfo()");
-  getaddrinfo(hostname, port, &hints, &res);
-  
-  /* We should really be looping here. */
-  DEBUG("calling socket()");
-  clientfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-  
-  DEBUG("calling connect()");
-  connect(clientfd, res->ai_addr, res->ai_addrlen);
-  
-  freeaddrinfo(res);
-  return 0;
-}
-
-int netc_auth(const char *user, const char *pass) {
-  return 0;
-}
-
-void netc_say(char *msg) {
-  cot_packet_t pkt;
-  
-  /* Prepare packet. */
-  pkt.type = PKT_SAY;
-  pkt.len  = strlen(msg);
-  pkt.data = (uint8_t*)msg;
-  
-  net_send(clientfd, &pkt);
-}
-
-void netc_sayf(const char *msg, ...) {
-}
+#endif
 
