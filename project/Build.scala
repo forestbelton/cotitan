5fd3fead1de03bbd7a1a112f3664d16229f3c8c7
Muhtasim Ahsan <agro@jantox.com>
2015-05-24 18:26:06 -0500

    add in websocket dependency.

--- project/Build.scala
+++ project/Build.scala
@@ -5,21 +5,23 @@ import spray.revolver.RevolverPlugin._
 
 object Build extends Build {
 
-  lazy val root = (project in file("."))
-    .settings(Revolver.settings: _*)
-    .settings(
-      name                  := "server",
-      organization          := "com.cotitan",
-      version               := "0.1.0",
-      scalaVersion          := "2.11.4",
-      licenses              += ("MIT", url("http://opensource.org/licenses/MIT")),
-      scalacOptions         += "-feature",
-      scalacOptions         += "-deprecation",
-      scalacOptions         += "-unchecked",
-      scalacOptions         += "-language:implicitConversions",
-      resolvers             += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
-      libraryDependencies   += "com.typesafe.akka" % "akka-actor" % "2.0"
-    )
+    lazy val root = (project in file("."))
+        .settings(Revolver.settings: _*)
+        .settings(
+            name                  := "server",
+            organization          := "com.cotitan",
+            version               := "0.1.0",
+            scalaVersion          := "2.11.4",
+            licenses              += ("MIT", url("http://opensource.org/licenses/MIT")),
+            scalacOptions         += "-feature",
+            scalacOptions         += "-deprecation",
+            scalacOptions         += "-unchecked",
+            scalacOptions         += "-language:implicitConversions",
+            resolvers             += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+            resolvers             += "Spray" at "http://repo.spray.io",
+            libraryDependencies   += "com.typesafe.akka" % "akka-actor" % "2.0",
+            libraryDependencies   += "com.wandoulabs.akka" %% "spray-websocket" % "0.1.4"
+        )
 
 }
 
