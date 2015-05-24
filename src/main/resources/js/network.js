4b3f990a8953661a568e7261584085c200edc13a
Forest Belton <forest@homolo.gy>
2015-05-24 16:49:58 -0700

    add network module

---
+++ src/main/resources/js/network.js
@@ -0,0 +1,30 @@
+var socket = null;
+var cbs    = [];
+
+export default {
+    init: function(url) {
+        if(socket != null) {
+            throw new Error('already initialized');
+        }
+
+        socket = new WebSocket(url);
+        socket.onmessage = event => {
+            const json = JSON.parse(event.data),
+                type = json.type,
+                data = json.data,
+                now = new Date();
+
+            console.log(`${now} [NET] ${type} -> ${JSON.stringify(data)}`);
+            cbs
+                .filter(cb => cb.type == type)
+                .forEach(cb => cb.func(data));
+        };
+    },
+
+    on: function(type, func) {
+        cbs.push({
+            type,
+            func
+        });
+    }
+};
