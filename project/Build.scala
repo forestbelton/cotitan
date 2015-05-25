fad40ace2dfbe89459e36012ad42b2efd8841cfa
Muhtasim Ahsan <agro@jantox.com>
2015-05-24 20:57:35 -0500

    add json dependency.

--- project/Build.scala
+++ project/Build.scala
@@ -8,19 +8,21 @@ object Build extends Build {
     lazy val root = (project in file("."))
         .settings(Revolver.settings: _*)
         .settings(
-            name                  := "server",
-            organization          := "com.cotitan",
-            version               := "0.1.0",
-            scalaVersion          := "2.11.4",
-            licenses              += ("MIT", url("http://opensource.org/licenses/MIT")),
-            scalacOptions         += "-feature",
-            scalacOptions         += "-deprecation",
-            scalacOptions         += "-unchecked",
-            scalacOptions         += "-language:implicitConversions",
-            resolvers             += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
-            resolvers             += "Spray" at "http://repo.spray.io",
-            libraryDependencies   += "com.typesafe.akka" % "akka-actor" % "2.0",
-            libraryDependencies   += "com.wandoulabs.akka" %% "spray-websocket" % "0.1.4"
+            name                := "server",
+            organization        := "com.cotitan",
+            version             := "0.1.0",
+            scalaVersion        := "2.11.4",
+            licenses            += ("MIT", url("http://opensource.org/licenses/MIT")),
+            scalacOptions       += "-feature",
+            scalacOptions       += "-deprecation",
+            scalacOptions       += "-unchecked",
+            scalacOptions       += "-language:implicitConversions",
+            resolvers           += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+            resolvers           += "Plasma Conduit Repository" at "http://dl.bintray.com/plasmaconduit/releases",
+            resolvers           += "Spray" at "http://repo.spray.io",
+            libraryDependencies += "com.typesafe.akka" % "akka-actor" % "2.0",
+            libraryDependencies += "com.wandoulabs.akka" %% "spray-websocket" % "0.1.4",
+            libraryDependencies += "com.plasmaconduit" %% "json" % "0.19.0"
         )
 
 }
